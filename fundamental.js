2a75ce45bfb4532281f533947fbe0be4c0ffd531
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-13 14:01:59 +0600

    2

--- fundamental.js
+++ fundamental.js
@@ -26,4 +26,19 @@ function multiply(num1, num2) {
 }
 
 // call the function
-const output = multiply();
+const output = multiply(35, 78);
+
+// 6.object 
+// 3 ways to access property by name in object
+const student = {
+    name: 'Salib khan',
+    age: 32,
+    movies: ['King khan', 'Dhakar Mastan'],
+}
+// 1.
+console.log(student.age); // direct by property
+// 2.
+console.log(student['age']); // access via property name string 
+// 3.
+const myVariable = 'age';
+console.log(student[myVariable]); // access via property name in a variable
