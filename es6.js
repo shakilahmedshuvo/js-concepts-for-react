ce8d14c117dcd36ad1f89b3fc5f2a0ac7bbc5123
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-13 16:23:07 +0600

    3

---
+++ es6.js
@@ -0,0 +1,9 @@
+const numbers = [89, 35, 98, 12];
+const student = {
+    name: 'Salib khan',
+    age: 32,
+    movies: ['King khan', 'Dhakar Mastan'],
+}
+// 
+const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
+console.log(about);
