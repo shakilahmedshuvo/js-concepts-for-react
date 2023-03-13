c05ea63959fb669ffdbdf30c6f2b917b4741d17d
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-03-14 00:26:11 +0600

    5

---
+++ distructuring.js
@@ -0,0 +1,10 @@
+// 1. arraydestaturing
+const numbers = [42, 65];
+
+// old
+// const x = numbers[0];
+// const y = numbers[1];
+// new
+const [x, y] = [42, 65];
+
+console.log(x, y);
--- es6.js
+++ es6.js
@@ -4,6 +4,27 @@ const student = {
     age: 32,
     movies: ['King khan', 'Dhakar Mastan'],
 }
-// 
+// 1. template string
 const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
 console.log(about);
+
+// 2.arrow function
+const getFiftyFive = () => 55;
+const addSixtyFive = num => num + 65;
+const isEven = x => x % 2 == 0;
+const addThree = (x, y, z) => x + y + z;
+// multiline function
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+
+// spread operator
+const newNumbers = [...numbers];
+numbers.push(99);
+// create a new araay form an older array and an element
+const currentNumbers = [...numbers, 55];
+
+console.log(numbers);
+console.log(newNumbers);
+console.log(currentNumbers);
