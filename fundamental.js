233e1a007291f2475f2d4ea90ced2951f170900b
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-13 13:43:07 +0600

    1

--- fundamental.js
+++ fundamental.js
@@ -8,4 +8,22 @@ else {
     i = o;
 }
 // 3.array
-const number = [89, 35, 98, 12];
+// index
+// length.push.pop
+const numbers = [89, 35, 98, 12];
+numbers[0] = 56;
+
+// 4.for loop
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+}
+
+// 5.function
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+
+// call the function
+const output = multiply();
